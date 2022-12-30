2d15c752318c335a7dbf9ae641a7f3191280d7c8
Diego Jevon de Lima Maia <diegojevon.delimamaia@edu.sait.ca>
2022-12-30 16:03:39 -0700

    Some changes for lg version into portfolio section

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -43,11 +43,11 @@ function Portfolio(){
                                 <Project1 />
                             </div>
                             <div className="my-4 box-content text-justify inline-block">
-                                <p className="py-4 text-lg max-sm:text-md">Inventory Web App. It's able to handle multiple inventory systems, for multiple companies, using the same Web App. Each company has different access, in different systems, not allowing the company's admin access other companies' inventory or user accounts ouside the admin's company. The Web App admin can  access all companies' inventory and all user accounts. </p>
+                                <p className="py-4 text-lg max-sm:text-md lg:h-40">Inventory Web App. It's able to handle multiple inventory systems, for multiple companies, using the same Web App. Each company has different access, in different systems, not allowing the company's admin access other companies' inventory or user accounts ouside the admin's company. The Web App admin can  access all companies' inventory and all user accounts. </p>
                             
                             
                                 <p className="mt-4 text-center">Technologies used by me in this project:</p>
-                                <div className="inline-flex max-md:inline-block gap-2 justify-between items-center pb-10">
+                                <div className="inline-flex max-md:inline-block justify-between items-center pb-10 w-full lg:pt-4">
                                     <Java />
                                     <Bootstrap />   
                                     <HTML />            
@@ -66,12 +66,12 @@ function Portfolio(){
                             <div className="pt-5">
                                 <Project2 />
                             </div>
-                            <div className="my-4 box-content text-justify inline-block">
-                                <p className="py-4 text-lg max-sm:text-md">All-in-one platform for digital nomads. My role was being part of the back-end team, responsable for web scraping, consumable APIs, database development and deployment.</p>
+                            <div className="my-4 box-content text-justify inline-block ">
+                                <p className="py-4 text-lg max-sm:text-md lg:h-40">All-in-one platform for digital nomads. My role was being part of the back-end team, responsable for web scraping, consumable APIs, database development and deployment.</p>
                             
                             
                                 <p className="mt-4 text-center">Technologies used by me in this project:</p>
-                                <div className="inline-flex max-md:inline-block gap-2 justify-between items-center pb-10 w-full">
+                                <div className="inline-flex max-md:inline-block justify-between items-center pb-10 w-full lg:pt-4">
                                     <Python />
                                     <Scrapy />
                                     <Node />            
